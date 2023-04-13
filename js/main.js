3a033c393ab8e1bc3d61216c409b6933cda6fd29
Master Erik <davidmasterkire@gmail.com>
2023-04-13 01:50:26 -0600

    Terminando logica de programacion de los botones de encriptar desencriptar, tambien se agregaron estilos a los botones al dar click

---
+++ js/main.js
@@ -0,0 +1,91 @@
+const textoEntrada = document.getElementById("textoEntrada");
+const btnEncriptar = document.getElementById("btn-encriptar");
+const btnDesencriptar = document.getElementById("btn-desencriptar");
+const textoSalida = document.getElementById("textoSalida");
+const contenedorSinDatos = document.querySelector(".cont-sin_Datos");
+const contenedorConDatos = document.querySelector(".cont-con_Datos");
+const btnCopy = document.getElementById("copy");
+let textoEncriptado = "";
+let textoDesencriptado = "";
+btnEncriptar.onclick = () => {
+  console.log("encriptando");
+  if (textoEntrada.value) {
+    let entrada = textoEntrada.value.toLowerCase();
+
+    contenedorSinDatos.classList.add("inactive");
+    contenedorConDatos.classList.remove("inactive");
+
+    textoEncriptado = entrada
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
+
+    textoSalida.textContent = textoEncriptado;
+    textoEntrada.value = "";
+  } else {
+    contenedorSinDatos.classList.remove("inactive");
+    contenedorConDatos.classList.add("inactive");
+  }
+};
+
+btnDesencriptar.onclick = () => {
+  console.log("desencriptando");
+  if (textoEntrada.value) {
+    let entrada = textoEntrada.value.toLowerCase();
+
+    contenedorSinDatos.classList.add("inactive");
+    contenedorConDatos.classList.remove("inactive");
+
+    textoDesencriptado = entrada
+      .replace(/enter/g, "e")
+      .replace(/imes/g, "i")
+      .replace(/ai/g, "a")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");
+
+    textoSalida.textContent = textoDesencriptado;
+    textoEntrada.value = "";
+  } else {
+    contenedorSinDatos.classList.remove("inactive");
+    contenedorConDatos.classList.add("inactive");
+  }
+};
+
+function copy() {
+  textoEntrada.value = textoSalida.textContent;
+  textoEntrada.select();
+  textoEntrada.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  textoEntrada.value = "";
+}
+
+btnCopy.addEventListener("click", copy);
+
+/* efectos */
+
+btnEncriptar.onmousedown = () => {
+    btnEncriptar.classList.add("click");
+}
+
+btnEncriptar.onmouseup = () => {
+    btnEncriptar.classList.remove("click");
+}
+
+btnDesencriptar.onmousedown = () => {
+    btnDesencriptar.classList.add("click");
+}
+
+btnDesencriptar.onmouseup = () => {
+    btnDesencriptar.classList.remove("click");
+}
+
+btnCopy.onmousedown = () => {
+    btnCopy.classList.add("click");
+}
+
+btnCopy.onmouseup = () => {
+    btnCopy.classList.remove("click");
+}
+
